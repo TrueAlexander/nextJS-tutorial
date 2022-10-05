f9ee7d3cf1470a29a6596783c2ff68d9ef1d48e0
Alexander <caminante.msk@gmail.com>
2022-10-05 19:34:03 -0300

    init tutorial

---
+++ components/MainContainer.js
@@ -0,0 +1,29 @@
+import Head from "next/head"
+import A from "../components/A"
+const MainContainer = ({children, keywords}) => {
+  return (
+    <>
+      <Head>
+        <meta keywords={"key, word, nextJS" + keywords}></meta>
+        <title>Main Page</title>
+      </Head>
+      <div className="navbar">
+        <A href="/" text="Main" />
+        <A href="/users" text="Users" />
+      </div>
+      <div>
+        {children}
+      </div>
+      <style jsx>
+        {`
+          .navbar {
+            background: orange;
+            padding: 15px;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default MainContainer
---
+++ pages/404.js
@@ -0,0 +1,7 @@
+export default function Error() {
+  return (
+    <h1>
+      My 404 Error Page
+    </h1>
+  )
+}
